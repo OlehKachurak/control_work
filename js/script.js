d0cc63ba0d0852ac00ab450fdaeeb9367eb44727
Oleh Kachurak <olegxpress7@gmail.com>
2021-10-25 22:27:45 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+const tasks = [
+  { id: 1, title: "Go to the cinema", done: false },
+  { id: 2, title: "Go to the theatre", done: true },
+  { id: 3, title: "Learn Java Script", done: false },
+  { id: 4, title: "Finish HTML project", done: false },
+];
+
+
+function renderTasks() {
+  const taskElements= tasks.map(function(item){
+    return `
+    <li class="list-group-item  d-flex">
+      <span>${item.title}</span>
+      <div class="icons-box ml-auto">
+        ${item.done ? `<i class="fa fa-check mr-3"></i>`:""}
+        <i  class="fa fa-trash"></i>
+      </div>
+    </li>`
+  }).join('');
+  document.getElementById("tasks-box").innerHTML = taskElements;
+}
+
+renderTasks()
+const form = document.getElementById('task-form')
+form.addEventListener('submit',function(event){
+  event.preventDefault();
+const formData = new FormData(form)
+  const title = formData.get('title'); 
+ 
+  tasks.push({ 
+    id: 5, 
+    title,
+    done: false
+ 
+  }) 
+  renderTasks();
+ } )
+ 
+ const total = document.getElementById('tasks-total').innerHTML= tasks.length;
+
+ const selectElement = document.querySelector('filter-tasks');
+
+ selectElement.addEventListener('change', (event) => {
+   const result = document.querySelector('tasks-box');
+
+   if(tasks.)
+
+
+   result.textContent = `You like ${event.target.value}`;
+ });
+ 
